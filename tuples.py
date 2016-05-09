b04c3ae128dc2630c246cf7a6f8e09b9caefc03b
mauriciorey <mauricio.rey@hotmail.com>
2016-05-09 16:44:08 -0400

    default tuple

--- tuples.py
+++ tuples.py
@@ -16,3 +16,14 @@ if 1 in tuple3:
     print "good"
 else:
     print "nothing"
+
+# Calculating Manhattan Distance
+from collections import namedtuple
+
+vector = namedtuple('Dimension', 'x,y,z')
+vec1 = vector(1,1,1)
+vec2 = vector(1,0,1)
+
+distance = abs(vec1.x - vec2.x) + abs(vec1.y - vec2.y) + abs(vec1.z - vec1.z)
+
+print 'Manhattan distance is %d' %(distance)
