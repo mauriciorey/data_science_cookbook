ee729ff8c3cbf6156f84197e68fede0956e0ab39
mauriciorey <mauricio.rey@hotmail.com>
2016-05-10 20:59:10 -0400

    Adding functions as parameters

--- embedded_functions.py
+++ embedded_functions.py
@@ -5,3 +5,18 @@ def sum_squares(x):
     return sum([square_input(value) for value in x])
 
 print sum_squares([1,2,3,5,6,7,8,9,10])
+
+#Passing a function as a parameter
+
+from math import log
+
+def input_square(x):
+    return x*x
+
+def apply_func(func_x, input_x):
+    return map(func_x, input_x)
+    
+a = [2,4,6]
+
+print apply_func(input_square, a)
+print apply_func(log, a)
