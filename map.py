08a22587bc5d20ddc161b816f5b663fa8ea87bc1
mauriciorey <mauricio.rey@hotmail.com>
2016-05-11 22:40:44 -0400

    maps and filters

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-
+# Working with maps
 a = [10,20,30]
 
 print map(lambda x: x**2, a)
@@ -7,3 +7,6 @@ print sum(map(lambda x: x**3, a))
 b = [1,2,3]
 
 print map(pow, a, b)
+
+# Working with filters
+print filter(lambda x: x > 15, a)
